648095030e41d4ac6eb8bb707ba455854243f8ad
Diego Pacori <dpacoria@unsa.edu.pe>
2024-02-28 12:01:02 -0500

    Se creo el seed y la base de datos con docker+prisma+postgres y RESTfull Api con Next

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,32 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+
+    await prisma.todo.deleteMany(); // delete * from todo
+
+    await prisma.todo.createMany({
+        data: [
+            {
+                description: 'Piedra del alma', complete: true
+            },
+            {
+                description: 'Piedra del poder'
+            },
+            {
+                description: 'Piedra del tiempo'
+            },
+            {
+                description: 'Piedra del espacio'
+            },
+            {
+                description: 'Piedra de la realidad'
+            },
+        ]
+    });
+
+    return NextResponse.json({
+        message: 'Seed Executed',
+    });
+}
+
