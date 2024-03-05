4789998b62ab4b182311b9029f3d08f66697d620
Diego Pacori <dpacoria@unsa.edu.pe>
2024-03-05 18:01:08 -0500

    Se hizo el login con google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -7,6 +8,10 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID ?? '',
             clientSecret: process.env.GITHUB_SECRET ?? '',
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        })
     ],
 };
 
